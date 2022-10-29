7036dbed2cc6dc0c3863e344c433950968165797
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-29 14:29:27 +0600

    Создана форма продукта

---
+++ review_app/webapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from webapp.models import Product
+from django.forms import ValidationError
+
+
+
+def max_length_validator(string):
+    if len(string) > 20:
+        raise ValidationError('The max number of characters is 20')
+    return string
+
+
+def min_length_validator(string):
+    if len(string) < 4:
+        raise ValidationError('The minimum number of characters is 8')
+    return
+
+
+class ProductForm(forms.ModelForm):
+
+    name = forms.CharField(validators=(min_length_validator, max_length_validator))
+
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'category', 'image')
