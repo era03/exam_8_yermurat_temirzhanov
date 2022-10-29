6b51ef3f9aa667087e4dfe0a975779007fbe396f
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-29 14:28:06 +0600

    Созданы шаблоны для главной страницы, для создания, и для подробной информации продукта

--- review_app/webapp/urls.py
+++ review_app/webapp/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from webapp.views.base import IndexView
+from webapp.views.products import ProductDetailView, ProductCreateView
 
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name='index'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'), 
 ]
