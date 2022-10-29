2d1f479a70e419918d29c5a6f4e78a18cb1daeae
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-29 12:09:46 +0600

    Модели зарегестрированны в админ панели

--- review_app/webapp/admin.py
+++ review_app/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Product, Review
 
-# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'image')
+    list_filter = ('id', 'name', 'description', 'category')
+    search_fields = ('name', 'description', 'category')
+    fields = ('name', 'description', 'image', 'category')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'product', 'text', 'rating')
+    list_filter = ('id', 'author', 'product', 'text', 'rating')
+    search_fields = ('author', 'product', 'text', 'rating')
+    fields = ('author', 'product', 'text', 'rating')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
