4808b367e55826a8f79c99d0b532d87af36e342b
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-29 18:26:59 +0600

    Добавлена форма для авторизации, создания пользователя, изменения профиля, изменения пароля

---
+++ review_app/accounts/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True, label='Логин')
+    password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
+    next = forms.CharField(required=False, widget=forms.HiddenInput)
+
+
+class UserCreationForm(forms.ModelForm):
+
+    email = forms.EmailField(label='Email', required=True)
+    password = forms.CharField(label="Password", strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm password", required=True, widget=forms.PasswordInput, strip=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords don't match!")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label="New password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm password", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Old password", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords don't match!")
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('The old password is incorrect!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ['password', 'password_confirm', 'old_password']
